5a8bca8acbde0552ecc7e14f5c8867be4cc9ee32
afonscosta <afonso.costa.15@gmail.com>
2019-04-01 23:11:48 +0100

    Navbar as a component. DaySpan added

--- cuida24/src/main.js
+++ cuida24/src/main.js
@@ -9,13 +9,27 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery/src/jquery.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
-import '../public/static/offcanvas.css'
+import './assets/offcanvas.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
+
+import Vuetify from 'vuetify'
+import DaySpanVuetify from 'dayspan-vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'dayspan-vuetify/dist/lib/dayspan-vuetify.min.css'
 
 Vue.config.productionTip = false
 
 // Vue.use(VueRouter)
 Vue.use(BootstrapVue)
+
+Vue.use(Vuetify);
+
+Vue.use(DaySpanVuetify, {
+  methods: {
+    getDefaultEventColor: () => '#1976d2'
+  }
+});
 
 const vue = new Vue({
   router,
