f560f653c1cf3efd58a912e27d6671618b517581
Marco Silva <marant_silva@hotmail.com>
2019-04-07 22:42:20 +0100

    implementation of backend authentication (backend receives token request and return an access token / when this token expire, a refresh token request can be send to the backend to obtain a new access token)

--- cuida24/backend/settings/dev.py
+++ cuida24/backend/settings/dev.py
@@ -74,6 +74,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES' : ('rest_framework.permissions.IsAuthenticated',),
+    'DEFAULT_AUTHENTICATION_CLASSES' : ('rest_framework_simplejwt.authentication.JWTAuthentication',)
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
