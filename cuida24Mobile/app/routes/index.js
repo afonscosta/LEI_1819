2692512b32336ebc295b272c5150ee7fd265ee55
afonscosta <afonso.costa.15@gmail.com>
2019-04-25 12:26:34 +0100

    added color to drawer

--- cuida24Mobile/app/routes/index.js
+++ cuida24Mobile/app/routes/index.js
@@ -32,6 +32,9 @@ class NavigationDrawerStructure extends Component {
     );
   }
 }
+
+const bgColor = '#FFD185';
+const htColor = '#343F4B';
  
 const Calendar_StackNavigator = createStackNavigator({
   //All the screen from the Screen1 will be indexed here
@@ -41,9 +44,9 @@ const Calendar_StackNavigator = createStackNavigator({
       title: 'Calendário',
       headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
       headerStyle: {
-        backgroundColor: '#FF9800',
+        backgroundColor: bgColor,
       },
-      headerTintColor: '#fff',
+      headerTintColor: htColor,
     }),
   },
 });
@@ -56,9 +59,9 @@ const Medication_StackNavigator = createStackNavigator({
       title: 'Medicação',
       headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
       headerStyle: {
-        backgroundColor: '#FF9800',
+        backgroundColor: bgColor,
       },
-      headerTintColor: '#fff',
+      headerTintColor: htColor,
     }),
   },
 });
@@ -71,9 +74,9 @@ const Habits_StackNavigator = createStackNavigator({
       title: 'Hábitos',
       headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
       headerStyle: {
-        backgroundColor: '#FF9800',
+        backgroundColor: bgColor,
       },
-      headerTintColor: '#fff',
+      headerTintColor: htColor,
     }),
   },
 });
@@ -86,9 +89,9 @@ const Chat_StackNavigator = createStackNavigator({
       title: 'Chat',
       headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
       headerStyle: {
-        backgroundColor: '#FF9800',
+        backgroundColor: bgColor,
       },
-      headerTintColor: '#fff',
+      headerTintColor: htColor,
     }),
   },
 });
@@ -101,9 +104,9 @@ const Games_StackNavigator = createStackNavigator({
       title: 'Jogos',
       headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
       headerStyle: {
-        backgroundColor: '#FF9800',
+        backgroundColor: bgColor,
       },
-      headerTintColor: '#fff',
+      headerTintColor: htColor,
     }),
   },
 });
@@ -116,58 +119,61 @@ const Info_StackNavigator = createStackNavigator({
       title: 'Páginas Informativas',
       headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
       headerStyle: {
-        backgroundColor: '#FF9800',
+        backgroundColor: bgColor,
       },
-      headerTintColor: '#fff',
+      headerTintColor: htColor,
     }),
   },
 });
 
 const DrawerNavigator = createDrawerNavigator({
-  //Drawer Optons and indexing
-  Calendar: {
-    //Title
-    screen: Calendar_StackNavigator,
-    navigationOptions: {
-      drawerLabel: 'Calendário',
+    //Drawer Optons and indexing
+    Calendar: {
+      //Title
+      screen: Calendar_StackNavigator,
+      navigationOptions: {
+        drawerLabel: 'Calendário',
+      },
+    },
+    Medication: {
+      //Title
+      screen: Medication_StackNavigator,
+      navigationOptions: {
+        drawerLabel: 'Medicação',
+      },
+    },
+    Habits: {
+      //Title
+      screen: Habits_StackNavigator,
+      navigationOptions: {
+        drawerLabel: 'Hábitos',
+      },
+    },
+    Chat: {
+      //Title
+      screen: Chat_StackNavigator,
+      navigationOptions: {
+        drawerLabel: 'Chat',
+      },
+    },
+    Games: {
+      //Title
+      screen: Games_StackNavigator,
+      navigationOptions: {
+        drawerLabel: 'Jogos',
+      },
+    },
+    Info: {
+      //Title
+      screen: Info_StackNavigator,
+      navigationOptions: {
+        drawerLabel: 'Páginas informativas',
+      },
     },
   },
-  Medication: {
-    //Title
-    screen: Medication_StackNavigator,
-    navigationOptions: {
-      drawerLabel: 'Medicação',
-    },
-  },
-  Habits: {
-    //Title
-    screen: Habits_StackNavigator,
-    navigationOptions: {
-      drawerLabel: 'Hábitos',
-    },
-  },
-  Chat: {
-    //Title
-    screen: Chat_StackNavigator,
-    navigationOptions: {
-      drawerLabel: 'Chat',
-    },
-  },
-  Games: {
-    //Title
-    screen: Games_StackNavigator,
-    navigationOptions: {
-      drawerLabel: 'Jogos',
-    },
-  },
-  Info: {
-    //Title
-    screen: Info_StackNavigator,
-    navigationOptions: {
-      drawerLabel: 'Páginas informativas',
-    },
-  },
-});
+  {
+    drawerBackgroundColor: bgColor,
+  });
 
 const AppContainer = createAppContainer(DrawerNavigator);
  
