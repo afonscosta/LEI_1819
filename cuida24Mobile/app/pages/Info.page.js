784dd8dce4ba554c1d89b3421e142c398a10e2fc
Marco Silva <marant_silva@hotmail.com>
2019-05-25 16:35:47 +0100

    migrating to dev

--- cuida24Mobile/app/pages/Info.page.js
+++ cuida24Mobile/app/pages/Info.page.js
@@ -2,6 +2,17 @@ import React from 'react'
 import { StyleSheet, Text, View, Image } from 'react-native'
 
 export default class InfoPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state  = {
+      info: {},
+      loading: false,
+      error: null,
+      refreshing: false,
+      base_url: "http://10.0.2.2:8000/cuida24/"
+    }
+  }
+
   static navigationOptions = {
     drawerLabel: 'Info Page',
     drawerIcon: () => (
@@ -12,10 +23,39 @@ export default class InfoPage extends React.Component {
     )
   }
 
+  // componentDidMount() {
+  //   const url = this.state.base_url + "staticPages";
+
+  //   this.setState({ loading: true });
+
+  //   fetch(url)
+  //     .then(res => res.json())
+  //     .then(res => {
+  //       this.setState({
+  //         info: res,
+  //         error: null,
+  //         loading: false,
+  //         refreshing: false
+  //       });
+  //     })
+  //     .catch(error => {
+  //       this.setState({ error, loading : false });
+  //     });
+  // }
+
   render() {
+    const elems = {'titulo1': 'adjshvbejhrv', 'titulo2': 'ajdhvejfhv'};
+
+    const items = [];
+
+    for ((key, value) of elems) {
+      items.push(<h2> {index} </h2> )
+      // elems.push(<p> {value} </p>)
+    }
+
     return (
       <View style={styles.container}>
-        <Text>Info page</Text>
+        {items}
       </View>
     )
   }
