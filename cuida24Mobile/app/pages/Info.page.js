788f990fefe83b5698ee863a30f027e71e5321dc
Marco Silva <marant_silva@hotmail.com>
2019-05-28 23:41:52 +0100

    new info page element to display html code

--- cuida24Mobile/app/pages/Info.page.js
+++ cuida24Mobile/app/pages/Info.page.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { StyleSheet, ScrollView, Image, Text } from 'react-native'
+import { StyleSheet, FlatList, Image, Text } from 'react-native'
+import { List } from 'react-native-elements';
 import HTML from 'react-native-render-html';
-import Scrollspy from 'react-scrollspy'
 
 export default class InfoPage extends React.Component {
   constructor(props) {
@@ -12,9 +12,7 @@ export default class InfoPage extends React.Component {
       error: null,
       refreshing: false,
       base_url: "http://10.0.2.2:8000/cuida24/",
-      refs: {}
     }
-    this.setScrollViewRef = React.createRef();
   }
 
   static navigationOptions = {
@@ -47,33 +45,42 @@ export default class InfoPage extends React.Component {
       });
   }
 
-  scrollToTitle(title) {
-    this.state.refs[title].measure((fx, fy, width, height, px, py) => {
-      this.setScrollViewRef.scrollTo({x:0, y: 0, animated: true})
-    })
-  }
+  // scrollToTitle(title) {
+  //   this.state.refs[title].measure((fx, fy, width, height, px, py) => {
+  //     this.setScrollViewRef.scrollTo({x:0, y: 0, animated: true})
+  //   })
+  // }
+
+//   setItemRef= (itemId) => (element) => 
+// {
+//  const {selectedItemId} = this.props.navigation.state.params;
+//  if(selectedItemId === itemId && !this.itemRef)
+//     this.itemRef = element;
+// }
+
+//   setScrollViewRef = (element) => {
+//     this.scrollViewRef = element;
+// };
 
   render() {
     const elems = this.state.info;
 
-    for (var key in elems) {
-      this.state.refs[key] = React.createRef();
-    }
-
     return (
-      <ScrollView ref={this.setScrollViewRef} style={{ flex: 1 }} contentContainerStyle={styles.container}>
-
-          {/* {Object.keys(elems).map(key => (
-            <Text style={{color: 'blue'}} onPress={this.scrollToTitle(elems[key].title)} >
-              {elems[key].title}
-            </Text>
-          ))} */}
-
-        {Object.keys(elems).map(key => (
-          <HTML ref={this.state.refs[elems[key].title]} key={key} html={ '<h3>' + elems[key].title + '</h3>' + elems[key].text } />
-        ))}
-      </ScrollView>
+      <List>
+        <FlatList
+          data={elems}
+          renderItem={({ item }) => (
+            <HTML html={ '<h3>' + item.title + '</h3>' + item.text } />
+          )}
+        />
+      </List>
     )
+
+          // {Object.keys(elems).map(key => (
+          //   <Text style={{color: 'blue'}} onPress={this.scrollToTitle(elems[key].title)} >
+          //     {elems[key].title}
+          //   </Text>
+          // ))}
   }
 }
 
