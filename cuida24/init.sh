b12062fc0b9c97bb29d67623382bd90ae6d95def
Patrícia Barreira <patriciafbarreira@gmail.com>
2019-06-22 19:32:49 +0100

    ao registar um utilizador e associado aos grupos que e suposto

--- cuida24/init.sh
+++ cuida24/init.sh
@@ -4,6 +4,6 @@ psql -U postgres -f updatedb.sql
 rm backend/cuida24/migrations/0002*
 python manage.py makemigrations
 python manage.py migrate
-#python manage.py initdata
-python manage.py loaddata initial_data.json initial_data_users.json 
+python manage.py initdata
+#python manage.py loaddata backend/cuida24/fixtures/initial_data.json  backend/cuida24/fixtures/initial_data_users.json --verbosity 
 #python manage.py createsuperuser
